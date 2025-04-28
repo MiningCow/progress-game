cf82bdc75765014b392ee3d38a9f43e21e28e8cc
MiningCow <lev.gluzdov@gmail.com>
2025-04-27 23:42:45 -0400

    multiplayer WIP

--- Things/game/game.gd
+++ Things/game/game.gd
@@ -1,1 +1,13 @@
 extends Node
+
+signal game_started
+
+func _on_host_pressed():
+	start_game()
+
+func _on_join_pressed():
+	pass # Replace with function body.
+
+func start_game():
+	game_started.emit()
+	$UILayer/Menu.hide()
